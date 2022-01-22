3050d428151640b59565d4932bd2f7cb5b484319
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2022-01-22 10:56:08 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,83 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello Vue 2 + Vite" />
+  <div>
+    <div style="margin: 1rem 0;">
+      <Logo />
+    </div>
+
+    <h2>Hello {{ user.name }}</h2>
+
+    <form @submit.prevent="addItemToCart" data-testid="add-items">
+      <input type="text" v-model="itemName" />
+      <button>Add</button>
+    </form>
+
+    <form @submit.prevent="buy">
+      <ul data-testid="items">
+        <li v-for="item in cart.items" :key="item.name">
+          {{ item.name }} ({{ item.amount }})
+          <button
+            @click="cart.removeItem(item.name)"
+            type="button"
+          >X</button>
+        </li>
+      </ul>
+
+      <button :disabled="!user.name">Buy</button>
+      <button
+        :disabled="!cart.items.length"
+        @click="clearCart"
+        type="button"
+        data-testid="clear"
+      >Clear the cart</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-export default {
-  components: {
-    HelloWorld,
+import Logo from './components/Logo.vue'
+import { defineComponent, ref } from 'vue-demi'
+import { useUserStore } from './stores/user'
+import { useCartStore } from './stores/cart'
+export default defineComponent({
+  components: { Logo },
+  setup() {
+    const user = useUserStore()
+    const cart = useCartStore()
+    const itemName = ref('')
+    function addItemToCart() {
+      if (!itemName.value) return
+      cart.addItem(itemName.value)
+      itemName.value = ''
+    }
+    function clearCart() {
+      if (window.confirm('Are you sure you want to clear the cart?')) {
+        cart.rawItems = []
+      }
+    }
+    async function buy() {
+      const n = await cart.purchaseItems()
+      console.log(`Bought ${n} items`)
+      cart.rawItems = []
+    }
+    return {
+      itemName,
+      addItemToCart,
+      cart,
+      user,
+      buy,
+      clearCart,
+    }
   },
-};
+})
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+img {
+  width: 200px;
+}
+button,
+input {
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-const { createVuePlugin } = require('vite-plugin-vue2');
+import { createVuePlugin as vue } from 'vite-plugin-vue2'
+import ViteComponents from "vite-plugin-components";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [createVuePlugin()],
+  plugins: [vue(),ViteComponents({ transformer: "vue2" }),],
+  resolve: {
+    alias: {
+      "@": `${resolve(__dirname, "src")}`,
+    },
+  },
 })
